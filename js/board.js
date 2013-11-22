cd6c3cfeaa9a4dd3a1d59645d8e49e5ed1301805
John Paul Welsh <wearing2raincoats@yahoo.com>
2013-11-22 13:18:12 -0500

    took out pawn.js, combined with board.js, saved chessboard as better name

--- js/board.js
+++ js/board.js
@@ -1,14 +1,21 @@
 var Board = function( loader ) {
-	this.init( loader );
-}
+	loader.load( 'objects/chessboard.obj', 'objects/chessboard.mtl', function ( object ) {
+	    object.scale.x = 20;
+	    object.scale.y = 20;
+	    object.scale.z = 20;
 
-Board.prototype.init = function( loader ) {
-    loader.load( 'objects/chessboardwhitenomarblejoin.obj', 'objects/chessboardwhitenomarblejoin.mtl', function ( object ) {
-        object.scale.x = 20;
-        object.scale.y = 20;
-        object.scale.z = 20;
+	    loader.load( 'whitepawn.obj', 'whitepawn.mtl', function ( object ) {
+	    	object.scale.x = 4;
+	        object.scale.y = 4;
+	        object.scale.z = 4;
+
+	        object.translateY( 3 );
+
+	        pawn = object;
+	        game.add( pawn );
+	    } );
 
 		board = object;
-        game.add( board );
+	    game.add( board );
 	} );
 }
