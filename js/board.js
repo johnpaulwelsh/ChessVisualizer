112267cb0365a7b7149547b6ec8a04faf0a7510d
John Paul Welsh <wearing2raincoats@yahoo.com>
2013-11-23 02:55:31 -0500

    added rook, it's rendering weirdly though

--- js/board.js
+++ js/board.js
@@ -5,17 +5,42 @@ var Board = function( loader ) {
 	    object.scale.z = 20;
 
 	    loader.load( 'objects/whitepawn.obj', 'objects/whitepawn.mtl', function ( object ) {
-	    	object.scale.x = 4;
-	        object.scale.y = 4;
-	        object.scale.z = 4;
+	    	object.scale.x = 3;
+	        object.scale.y = 3;
+	        object.scale.z = 3;
 
 	        object.translateY( 3 );
 
+	        loader.load( 'objects/whiterook.obj', 'objects/whiterook.mtl', function ( object ) {
+	        	object.scale.x = 3;
+	        	object.scale.y = 3;
+	        	object.scale.z = 3;
+
+	        	object.translateY( 30 );
+
+	        	rook = object;
+	        	game.add( rook );
+
+	        	rook.translateX( 25 );
+	        	rook.translateZ( 40 );
+	        } );
+
 	        pawn = object;
 	        game.add( pawn );
+
+            pawn.translateX( -77 );
+            pawn.translateZ( 22 );
 	    } );
 
 		board = object;
 	    game.add( board );
 	} );
-}
+};
+
+Board.prototype.movePiece = function( piece ) {
+	// move piece
+};
+
+Board.prototype.run = function(  ) {
+	// do stuff
+};
